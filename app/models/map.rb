9710637fc1de49d9c05fff54989c23fa9e356ad1
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-23 13:14:07 +0200

    add relationships between models

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,6 @@
 class Map < ApplicationRecord
   belongs_to :user
+  has_many :locations, through: :added_locations, dependent: :destroy
+  has_many :user_groups, dependent: :destroy
+  has_many :users, through: :user_groups
 end
