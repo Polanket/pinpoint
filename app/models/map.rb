983e6c504efde279033964835a41f434e3bc3b58
Juan <juanmpt@outlook.com>
2019-07-24 11:48:29 +0200

    Add map validations

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,6 @@ class Map < ApplicationRecord
   has_many :added_locations, dependent: :destroy
   has_many :user_groups
   has_many :users, through: :user_groups
+
+  validates :name, presence: true, allow_blank: false
 end
