6dc404de62470f5d26d2761cae727275f7073c2b
Juan <juanmpt@outlook.com>
2019-07-24 11:36:09 +0200

    Add review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :added_location
   belongs_to :user
+
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true, less_than: 6 }, inclusion: { in: [1, 2, 3, 4, 5] }
 end
