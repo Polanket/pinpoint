338702dddbd06055deddc6dfd6c69c22ca3acbab
Juan <juanmpt@outlook.com>
2019-07-24 17:04:45 +0200

    Remove google maps JS Map API and use Mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
-import { initMap } from '../plugins/init_map';
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import { initMap } from '../plugins/init_map';
 
-initMap();
-window.initMap = initMap;
+// initMap();
+// window.initMap = initMap;
+
+import { initMapbox } from '../plugins/init_mapbox'
+
+initMapbox();
---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,31 @@
+import mapboxgl from 'mapbox-gl';
+
+const mapDiv = document.getElementById('map');
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const initMapbox = () => {
+
+  if (mapDiv) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+    const markers = JSON.parse(mapDiv.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers);
+  }
+};
+
+export { initMapbox };
