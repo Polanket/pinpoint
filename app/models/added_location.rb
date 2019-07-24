70c21670e7408d59e9fcd5d502dbcdb18dc7a35e
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-24 11:44:07 +0200

    add validations to added_locations

--- app/models/added_location.rb
+++ app/models/added_location.rb
@@ -1,4 +1,7 @@
 class AddedLocation < ApplicationRecord
   belongs_to :map
   has_many :tags
+
+  validates :name, :address, :description :photo presence: true, uniqueness: true
+  validates :longitude, :latitude, presence: true, uniqueness: true, allow_blank: false
 end
