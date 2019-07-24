fdbb5652ba0d59790ce85563f572ee58eb7ac07a
Juan <juanmpt@outlook.com>
2019-07-24 13:37:51 +0200

    Add google places autocomplete/search feature

--- app/javascript/plugins/init_map.js
+++ app/javascript/plugins/init_map.js
@@ -2,21 +2,49 @@
 const loadGoogleMapsApi = require('load-google-maps-api');
 
 const mapDiv = document.getElementById('map');
+const input = document.getElementById('search-box');
+
 const options = {
-  key: process.env.GOOGLE_API_KEY
+  key: process.env.GOOGLE_API_KEY,
+  libraries: ['places']
 };
+
+const initSearchBox = (element, gMapApi, map) => {
+  const searchBox = new gMapApi.places.SearchBox(element);
+  map.controls[gMapApi.ControlPosition.BOTTOM_CENTER].push(element);
+  map.addListener('bounds_changed', () => {
+    searchBox.setBounds(map.getBounds());
+  })
+}
+
+const initSearch = (search) => {
+  const places = new googleMaps.places.PlacesService(map);
+
+  places.findPlaceFromQuery(search, (results, status) => {
+    if (status === googleMaps.places.PlacesServiceStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
 
 const callMapAPI = (lat=40.4168, lng=-3.7038) => {
   loadGoogleMapsApi(options)
     .then((googleMaps) => {
-      new googleMaps.Map(mapDiv, {
+      const map = new googleMaps.Map(mapDiv, {
         center: {
           lat: lat,
           lng: lng
         },
         zoom: 16,
         disableDefaultUI: true
-    })
+    });
+      initSearchBox(input, googleMaps, map);
+
+      const search = {
+        query: input.value,
+        fields: ['name', 'geometry']
+      };
+
   }).catch(error => console.log(error));
 };
 
