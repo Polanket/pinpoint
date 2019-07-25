a4c248c83e183e467c52f3285125afcff0a5c843
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-25 13:26:41 +0200

    sidebar to push

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { initMapbox } from '../plugins/init_mapbox'
+import { initMapbox } from '../plugins/init_mapbox';
+
+import { initNav } from '../plugins/init_sidebar';
 
 initMapbox();
+
+initNav();
+
+
+
---
+++ app/javascript/plugins/init_sidebar.js
@@ -0,0 +1,27 @@
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+const openNav = () => {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+};
+
+const initNav = () => {
+  const openButton = document.getElementById("open-nav");
+  openButton.addEventListener("click", (event) => {
+    openNav();
+  })
+  const closeButton = document.getElementById("close-nav");
+  closeButton.addEventListener("click", (event) => {
+    closeNav();
+  })
+}
+
+
+
+
+export { initNav };
