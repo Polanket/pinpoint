caf27b0c5265d03efbe01c3212c843e5c820e360
Juan <juanmpt@outlook.com>
2019-07-25 13:15:51 +0200

    Added searchbox that communicates with google places

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,5 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox'
 
+global.initMapbox = initMapbox;
 initMapbox();
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
-  resources :users do
-    resources :maps
-  end
+  resources :maps
+  get 'maps/:id/view', to: 'maps#add_location', as: 'view_locations'
 end
