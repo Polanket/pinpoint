1662241a9c7d168638d60bd378e11c3a2901bbbb
Juan <juanmpt@outlook.com>
2019-07-26 13:22:52 +0200

    Urgent sidebar styling

--- app/javascript/plugins/init_sidebar.js
+++ app/javascript/plugins/init_sidebar.js
@@ -1,7 +1,7 @@
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 const openNav = () => {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.width = "350px";
   document.getElementById("main").style.marginLeft = "250px";
 };
 
