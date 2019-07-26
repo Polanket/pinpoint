0fabaffdb28b496a63c35499371fd8f1bddca789
Juan <juanmpt@outlook.com>
2019-07-26 14:22:27 +0200

    Change the order functions are called in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 import { initNav } from '../plugins/init_sidebar';
 
-global.initMapbox = initMapbox;
-
-initMapbox();
 
 initNav();
+
+initMapbox();
 
 
 
