e3503288bc91ea0d3967b222aafd512cf7e20bc4
Juan <juanmpt@outlook.com>
2019-07-27 20:23:17 +0200

    Last minute configuration

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,8 +3,8 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.email
+      # self.current_user = find_verified_user
+      # logger.add_tags 'ActionCable', current_user.email
     end
 
     protected
