e7a7b16a654248197f27ba4faa8b6fc15bc4a5a4
Juan <juanmpt@outlook.com>
2019-07-29 21:36:06 +0200

    Add open boolean to added_locations & request more details per API call

--- app/controllers/added_locations_controller.rb
+++ app/controllers/added_locations_controller.rb
@@ -2,14 +2,15 @@ class AddedLocationsController < ApplicationController
   def create
     authorize current_map
     location = GPClient.spot(params[:marker_id])
-    current_map.added_locations
-    .where(latitude: location.lat, longitude: location.lng)
-    .first_or_create(
-        name: location.name,
-        address: location.formatted_address,
-        description: "Placeholder description",
-        photo: location.photos[0].fetch_url(800)
-    )
+    current_map
+      .added_locations
+      .where(latitude: location.lat, longitude: location.lng)
+      .first_or_create(
+          name: location.name,
+          address: location.formatted_address,
+          description: "Placeholder description",
+          photo: location.photos[0].fetch_url(800)
+      )
     @markers = marker_composer(current_map).compose
   end
 
