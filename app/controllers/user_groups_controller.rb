12174ed2aaf6e5f52cb3cc834925637f03ae2cf5
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-30 15:23:39 +0200

    delete user_group files not needed anymore

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -4,20 +4,4 @@ class UserGroupsController < ApplicationController
     @user_group = UserGroup.all
   end
 
-  def new
-    # @map = Map.find(params[:map_id])
-    # @user_group = UserGroup.new
-    # authorize @user_group
-  end
-
-  def create
-    # @map = Map.find(params[:map_id])
-    # @user_group = @map.user_groups.where(user_group_params).first_or_create
-    # authorize @user_group
-    # respond_to { |format| format.js } # <-- will render `app/views/user_groups/create.js.erb`
-  end
-
-  def user_group_params
-    # params.require(:user_group).permit(:user_id)
-  end
 end
